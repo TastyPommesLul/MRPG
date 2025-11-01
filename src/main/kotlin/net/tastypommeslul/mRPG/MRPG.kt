c96ab7a7128484471f0424d5340d5263a5f02856
TastyPommesLul <ernstd.privat@gmail.com>
2025-11-01 03:01:26 +0100

    Initial commit

---
+++ src/main/kotlin/net/tastypommeslul/mRPG/MRPG.kt
@@ -0,0 +1,14 @@
+package net.tastypommeslul.mRPG
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MRPG : JavaPlugin() {
+
+  override fun onEnable() {
+    // Plugin startup logic
+  }
+
+  override fun onDisable() {
+    // Plugin shutdown logic
+  }
+}
